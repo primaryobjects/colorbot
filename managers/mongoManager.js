5be924e370552adfcd68f9444ff7024719dcdcc5
Kory Becker <kbecker@primaryobjects.com>
2020-06-16 16:40:07 -0400

    Update mongoManager.js

--- managers/mongoManager.js
+++ managers/mongoManager.js
@@ -1,4 +1,4 @@
-var mongo = require('mongoskin')
+var MongoClient = require('mongodb').MongoClient;
 	config = require('../config/config');
 
 MongoManager = {
@@ -6,12 +6,15 @@ MongoManager = {
 		var index = Math.floor(Math.random()*(250 - count)); // choose a random index
 
 		// Get collection.
-		mongo.db(config.mongo.connectionString).collection('image').find().limit(count).skip(index).toArray(function (err, items) {
-			if (!err && items != null) {
-				if (callback != null) {
-					callback(items);
+		MongoClient.connect(config.mongo.connectionString, function(err, db) {
+			var dbo = db.db('rgbimages');
+			dbo.collection('image').find().limit(count).skip(index).toArray(function (err, items) {
+				if (!err && items != null) {
+					if (callback != null) {
+						callback(items);
+					}
 				}
-			}
+			});
 		});
 	}
 };
